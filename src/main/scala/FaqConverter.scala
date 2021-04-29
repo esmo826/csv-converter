be4fdd4a5ee4ff3f14c50c9a533527ef26e92ab6
elee3389k <esmond_lee@cable.comcast.com>
2021-04-29 11:49:56 -0700

    Integrated decline command line options

--- src/main/scala/FaqConverter.scala
+++ src/main/scala/FaqConverter.scala
@@ -1,44 +1,56 @@
-package com.leellc.faq.app
+package com.leellc.faqconverter
 
-import cats.effect.{Blocker, ExitCode, IO, IOApp}
+import cats.effect.{Blocker, ContextShift, Sync, IO}
 import cats.implicits._
 import fs2.{Stream, io, text}
-import java.nio.file.Paths
+import java.nio.file.Path
 
-object FaqConverter extends IOApp {
+trait FaqConverter[F[_]] {
+
+  def convert(input: Path, output: Path): F[Unit]
+
+}
+
+object FaqConverter {
 
   case class FaqRecord(faqId: String, productId: String) {
     override def toString(): String = s"$faqId,$productId"
   }
 
-  val converter: Stream[IO, Unit] = Stream.resource(Blocker[IO]).flatMap { blocker =>
-    def recordStringToFaqRecords(recordString: String): List[FaqRecord] = {
-      val faqWithProducts: List[String] = recordString.split(",").map(_.trim).toList
-      val faqId = faqWithProducts.head
-      val productIds = faqWithProducts.tail
-      // val fixError1 =
-      //   productIds.flatMap(s => if (s.contains(".")) s.split(".").map(_.trim).toList else List(s))
-      // val fixError2 = fixError1.flatMap(s =>
-      //   if (s.startsWith("NP0") && s.size == 18) s.splitAt(9).productIterator.toList.map(_.toString)
-      //   else List(s)
-      // )
-      // fixError2.map(pid => FaqRecord(faqId, pid))
-      productIds.map(pid => FaqRecord(faqId, pid));
+  def apply[F[_]: Sync: ContextShift](blocker: Blocker): F[FaqConverter[F]] = {
+
+    Sync[F].delay {
+
+      new FaqConverter[F] {
+        override def convert(input: Path, output: Path): F[Unit] = {
+
+          def recordStringToFaqRecords(recordString: String): List[FaqRecord] = {
+            val faqWithProducts: List[String] = recordString.split(",").map(_.trim).toList
+            val faqId = faqWithProducts.head
+            val productIds = faqWithProducts.tail
+            // val fixError1 =
+            //   productIds.flatMap(s => if (s.contains(".")) s.split(".").map(_.trim).toList else List(s))
+            // val fixError2 = fixError1.flatMap(s =>
+            //   if (s.startsWith("NP0") && s.size == 18) s.splitAt(9).productIterator.toList.map(_.toString)
+            //   else List(s)
+            // )
+            // fixError2.map(pid => FaqRecord(faqId, pid))
+            productIds.map(pid => FaqRecord(faqId, pid));
+          }
+
+          val result: Stream[F, Unit] = io.file
+            .readAll[F](input, blocker, 4096)
+            .through(text.utf8Decode)
+            .through(text.lines)
+            .filter(s => !s.trim.isEmpty && !s.startsWith("//"))
+            .map(line => recordStringToFaqRecords(line).mkString("\n"))
+            .intersperse("\n")
+            .through(text.utf8Encode)
+            .through(io.file.writeAll(output, blocker))
+
+          result.compile.drain
+        }
+      }
     }
-
-    io.file
-      .readAll[IO](Paths.get("/Users/elee3389k/Downloads/test.csv"), blocker, 4096)
-      .through(text.utf8Decode)
-      .through(text.lines)
-      .filter(s => !s.trim.isEmpty && !s.startsWith("//"))
-      .map(line => recordStringToFaqRecords(line).mkString("\n"))
-      .intersperse("\n")
-      .through(text.utf8Encode)
-      .through(io.file.writeAll(Paths.get("/Users/elee3389k/Downloads/out.csv"), blocker))
-  }
-
-  def run(args: List[String]): IO[ExitCode] = {
-    converter.compile.drain.as(ExitCode.Success)
   }
 }
-
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,6 @@ import cats.implicits._
 import com.monovore.decline._
 import com.monovore.decline.effect._
 import java.nio.file.Path
-import com.leellc.faq.app.FaqConverter
 
 object Main
     extends CommandIOApp(
@@ -14,24 +13,16 @@ object Main
       header = "FAQ converter"
     ) {
 
-    //private val opts: Opts[Path] = Opts.argument[Path]("file")
     private val input: Opts[Path] = Opts.option[Path]("in", short = "i", metavar = "inputFile", help = "Input CSV file.")
     private val output: Opts[Path] = Opts.option[Path]("out", short = "o", metavar = "outFile", help = "Output CSV file.")
 
     override def main: Opts[IO[ExitCode]] = {
-        input.map { i =>
-            FaqConverter.converter.compile.drain.as(ExitCode.Success)
+        (input, output).mapN { (i, o) =>
+            Blocker[IO].use { blocker =>
+                FaqConverter[IO](blocker).flatMap { converter =>
+                    converter.convert(i, o)
+                }
+            }.as(ExitCode.Success)
         }
     }
-        // opts.map { path =>
-        // Blocker[IO]
-        //     .use { blocker =>
-        //     Slf4jLogger.create[IO].flatMap { implicit logger =>
-        //         Renderer[IO](blocker).flatMap { renderer =>
-        //         Analyzer.analyze[IO](blocker, path).flatMap(renderer.renderAnalysis)
-        //         }
-        //     }
-        // }
-        // .as(ExitCode.Success)
-
 }
